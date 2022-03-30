14b3718a8fbaaaa07e185d29e889741e7da76a67
louize0 <louizedizon@gmail.com>
2022-03-30 23:48:48 +0800

    Initial commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { environment as env } from 'src/environments/environment';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginImg = `${env.ASSETS}login-img.png`
+
+  constructor() { }
+  
+
+  ngOnInit(): void {}
+
+}
